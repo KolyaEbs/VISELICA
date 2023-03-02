3e3dd131c689a814bf5cac1b29f9df1531544d7c
KolyaEbs <126643022+KolyaEbs@users.noreply.github.com>
2023-03-02 15:00:46 +0500

    Add files via upload

---
+++ ВИСЕЛИЦА/VISELICA/VISELICA/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace VISELICA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(651, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Играть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(651, 451);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(193, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Дать ответ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(651, 126);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(218, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(651, 416);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(218, 29);
+            this.textBox2.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::VISELICA.Properties.Resources._0;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(626, 543);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.IndianRed;
+            this.label1.Location = new System.Drawing.Point(664, 284);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 37);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.IndianRed;
+            this.label2.Location = new System.Drawing.Point(667, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(234, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Помораев и Фёдорова";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(651, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(193, 54);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Задать слово";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(797, 520);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 31);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "пися";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox1.Location = new System.Drawing.Point(651, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(193, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 563);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ ВИСЕЛИЦА/VISELICA/VISELICA/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace VISELICA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string text;
+        public char[] word;
+        public int hp;
+        public int hp2;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    StreamReader streamReader = new StreamReader("I:\\244-ИСП\\244ИСП\\ОАиП\\Помораев Николай\\ВИСЕЛИЦА\\1.txt");
+                    while (streamReader.EndOfStream)
+                    {
+                        textBox1.Text = streamReader.ReadLine();
+                    }
+
+                    text = streamReader.ReadLine();
+                    streamReader.Close();
+                    hp = 0;
+                    pictureBox1.Image = new Bitmap("I:\\244-ИСП\\244ИСП\\ОАиП\\Помораев Николай\\ВИСЕЛИЦА\\VISELICA\\VISELICA\\bin\\Debug\\0.png");
+                    label1.Text = null;
+                    textBox1.Text = null;
+                    word = new char[text.Length];
+                    pictureBox1.Visible = true;
+                    break;
+
+                    case 1:
+                    StreamReader streamReader2 = new StreamReader("I:\\244-ИСП\\244ИСП\\ОАиП\\Помораев Николай\\ВИСЕЛИЦА\\2.txt");
+                    while (streamReader2.EndOfStream)
+                    {
+                        textBox1.Text = streamReader2.ReadLine();
+                    }
+
+                    text = streamReader2.ReadLine();
+                    streamReader2.Close();
+                    hp = 0;
+                    pictureBox1.Image = new Bitmap("I:\\244-ИСП\\244ИСП\\ОАиП\\Помораев Николай\\ВИСЕЛИЦА\\VISELICA\\VISELICA\\bin\\Debug\\0.png");
+                    label1.Text = null;
+                    textBox1.Text = null;
+                    word = new char[text.Length];
+                    pictureBox1.Visible = true;
+                    break;
+                case 2:
+                    StreamReader streamReader3 = new StreamReader("I:\\244-ИСП\\244ИСП\\ОАиП\\Помораев Николай\\ВИСЕЛИЦА\\3.txt");
+                    while (streamReader3.EndOfStream)
+                    {
+                        textBox1.Text = streamReader3.ReadLine();
+                    }
+
+                    text = streamReader3.ReadLine();
+                    streamReader3.Close();
+                    hp = 0;
+                    pictureBox1.Image = new Bitmap("I:\\244-ИСП\\244ИСП\\ОАиП\\Помораев Николай\\ВИСЕЛИЦА\\VISELICA\\VISELICA\\bin\\Debug\\0.png");
+                    label1.Text = null;
+                    textBox1.Text = null;
+                    word = new char[text.Length];
+                    pictureBox1.Visible = true;
+                    break;
+                case 3:
+                    StreamReader streamReader4 = new StreamReader("I:\\244-ИСП\\244ИСП\\ОАиП\\Помораев Николай\\ВИСЕЛИЦА\\4.txt");
+                    while (streamReader4.EndOfStream)
+                    {
+                        textBox1.Text = streamReader4.ReadLine();
+                    }
+
+                    text = streamReader4.ReadLine();
+                    streamReader4.Close();
+                    hp = 0;
+                    pictureBox1.Image = new Bitmap("I:\\244-ИСП\\244ИСП\\ОАиП\\Помораев Николай\\ВИСЕЛИЦА\\VISELICA\\VISELICA\\bin\\Debug\\0.png");
+                    label1.Text = null;
+                    textBox1.Text = null;
+                    word = new char[text.Length];
+                    pictureBox1.Visible = true;
+                    break;
+                case 4:
+                    StreamReader streamReader5 = new StreamReader("I:\\244-ИСП\\244ИСП\\ОАиП\\Помораев Николай\\ВИСЕЛИЦА\\5.txt");
+                    while (streamReader5.EndOfStream)
+                    {
+                        textBox1.Text = streamReader5.ReadLine();
+                    }
+
+                    text = streamReader5.ReadLine();
+                    streamReader5.Close();
+                    hp = 0;
+                    pictureBox1.Image = new Bitmap("I:\\244-ИСП\\244ИСП\\ОАиП\\Помораев Николай\\ВИСЕЛИЦА\\VISELICA\\VISELICA\\bin\\Debug\\0.png");
+                    label1.Text = null;
+                    textBox1.Text = null;
+                    word = new char[text.Length];
+                    pictureBox1.Visible = true;
+                    break;
+
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                label1.Text += '.';
+                word[i] = '.';
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            {
+                bool words = false;
+                int j = 0;
+                if (textBox2.Text.Length == 1)
+                {
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        if (textBox2.Text[0] == text[i])
+                        {
+                            words = true;
+                            hp2++;
+                            word[i] = text[i];
+                            label1.Text = null;
+                        }
+                    }
+                    if (words == true)
+                    {
+                        for (int i = 0; i < text.Length; i++)
+                        {
+                            label1.Text += word[i];
+                        }
+                        if (hp2 == text.Length)
+                        {
+                            MessageBox.Show("Ты победил!");
+                        }
+                    }
+                    else
+                    {
+                        hp++;
+                        string hpb = hp.ToString() + ".png";
+                        pictureBox1.Image = new Bitmap(hpb);
+                        if (hp == 5)
+                        {
+                            MessageBox.Show("Ты проиграл!");
+                            label1.Text = null;
+                            label1.Text = text;
+                        }
+                    }
+
+                }
+                else if (textBox2.Text.Length > 1) //   ПОМОРАЕВ НИКОЛАЙ И ФЁДОРОВА ДАРЬЯ
+                {
+                    if (textBox2.Text == text)
+                    {
+                        label1.Text = null;
+                        for (int i = 0; i < text.Length; i++)
+                        {
+                            label1.Text += text[i];
+                        }
+                        MessageBox.Show("Ты победил!");
+                    }
+                    else
+                    {
+                        hp = 5;
+                        string hpb = hp.ToString() + ".png";
+                        pictureBox1.Image = new Bitmap(hpb);
+                        MessageBox.Show("Ты проиграл!");
+                        label1.Text = null;
+                        label1.Text = text;
+                    }
+                }
+                textBox2.Text = null;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    StreamWriter write_text;
+                    FileInfo file = new FileInfo("I:\\244-ИСП\\244ИСП\\ОАиП\\Помораев Николай\\ВИСЕЛИЦА\\1.txt");
+                    write_text = file.CreateText();
+                    write_text.WriteLine(textBox1.Text);
+                    write_text.Close();
+                    textBox1.Text = null;
+                    break;
+
+                case 1:
+                    StreamWriter write_text2;
+                    FileInfo file2 = new FileInfo("I:\\244-ИСП\\244ИСП\\ОАиП\\Помораев Николай\\ВИСЕЛИЦА\\2.txt");
+                    write_text2 = file2.CreateText();
+                    write_text2.WriteLine(textBox1.Text);
+                    write_text2.Close();
+                    textBox1.Text = null;
+                    break;
+                case 2:
+                    StreamWriter write_text3;
+                    FileInfo file3 = new FileInfo("I:\\244-ИСП\\244ИСП\\ОАиП\\Помораев Николай\\ВИСЕЛИЦА\\3.txt");
+                    write_text3 = file3.CreateText();
+                    write_text3.WriteLine(textBox1.Text);
+                    write_text3.Close();
+                    textBox1.Text = null;
+                    break;
+                case 3:
+                    StreamWriter write_text4;
+                    FileInfo file4 = new FileInfo("I:\\244-ИСП\\244ИСП\\ОАиП\\Помораев Николай\\ВИСЕЛИЦА\\4.txt");
+                    write_text4 = file4.CreateText();
+                    write_text4.WriteLine(textBox1.Text);
+                    write_text4.Close();
+                    textBox1.Text = null;
+                    break;
+                case 4:
+                    StreamWriter write_text5;
+                    FileInfo file5 = new FileInfo("I:\\244-ИСП\\244ИСП\\ОАиП\\Помораев Николай\\ВИСЕЛИЦА\\5.txt");
+                    write_text5 = file5.CreateText();
+                    write_text5.WriteLine(textBox1.Text);
+                    write_text5.Close();
+                    textBox1.Text = null;
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Text = null;
+            label1.Text = null;
+            pictureBox1.Visible = false;
+            
+
+        }
+    }
+}
